5a3c472e9f7cbbb0c77fcd57632f526840cad5d7
vonru <enjay0714@gmail.com>
2018-09-07 12:01:05 -0400

    Fixed lightning

---
+++ Lightning.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightning : MonoBehaviour {
+
+    public AudioClip clip;
+    bool canFlicker = true;
+
+    private void Update()
+    {
+        StartCoroutine(Flicker());
+    }
+        
+    IEnumerator Flicker()
+    {
+        if (canFlicker)
+        {
+            canFlicker = false;
+            GetComponent<AudioSource>().PlayOneShot(clip);
+            GetComponent<Light>().enabled = true;
+            yield return new WaitForSeconds(Random.Range(0.1f, 0.4f));
+            GetComponent<Light>().enabled = false;
+            yield return new WaitForSeconds(Random.Range(0.1f, 10f));
+            canFlicker = true;
+        }
+    }
+}
